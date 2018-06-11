ab9cc36e1c7e60b73e2b7a2ab1f7f7a304f4784f
Teresa Fontanella De Santis <tere.fds@hotmail.com>
2018-06-10 23:23:38 -0300

    Add spark  job

---
+++ pyspark_counter.py
@@ -0,0 +1,22 @@
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+import json
+
+def main(topic_name):
+    sc = SparkContext(appName="PythonSparkStreamingKafka_RM_01")
+    sc.setLogLevel("WARN")
+
+    ssc = StreamingContext(sc, 10)
+
+    kafkaStream = KafkaUtils.createStream(ssc, 'localhost:2181', 'spark-streaming', {'twitterApp':1})
+
+    parsed = kafkaStream.map(lambda v: json.loads(v[1]))
+
+    parsed.count().map(lambda x:'Tweets in this batch: %s' % x).pprint()
+
+    ssc.start()
+    ssc.awaitTermination()
+
+
+main('twitterApp')
