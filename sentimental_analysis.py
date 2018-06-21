1519adf72109d2a11daf6b030f670ac207e62274
Teresa Fontanella De Santis <tere.fds@hotmail.com>
2018-06-20 22:45:57 -0300

    Add sentimental_analysis in meaning cloud

--- sentimental_analysis.py
+++ sentimental_analysis.py
@@ -3,15 +3,24 @@ from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 import json
 import argparse
+import requests
 
 
 def enrich_twitter_mc(mc_key, twitter):
     url_mc = "http://api.meaningcloud.com/sentiment-2.1?"
-    payload = "key=%s&lang=%s&of=json&txt=%s" % (mc_key, tweet_lang, tweet['text'].encode('utf-8'))
+    payload = "key=%s&lang=%s&of=json&txt=%s" % (mc_key, twitter['lang'], twitter['text'])
     response = requests.request("POST", url_mc+payload)
-    #date_object = datetime.strptime(tweet['created_at'][:20]+tweet['created_at'][25:], '%a %b %d %H:%M:%S %Y')
-    tweet['created_at'] = datetime.strftime(date_object, '%Y-%m-%dT%H:%M:%S')
-    tweet['sent-analysis'] = json.loads(response.content.decode('utf-8'))
+    sentimental_analysis = json.loads(response.content.decode('utf-8'))
+    return json.dumps({
+        'twitter': twitter,
+        'sentimental_analysis': {
+            'score_tag': sentimental_analysis.get('score_tag'),
+            'agreement': sentimental_analysis.get('agreement'),
+            'subjectivity': sentimental_analysis.get('subjectivity'),
+            'confidence': sentimental_analysis.get('confidence'),
+            'irony': sentimental_analysis.get('irony')
+        }
+    })
 
 
 def main(
@@ -25,23 +34,25 @@ def main(
     ):
     sc = SparkContext(appName="StreamingSentimentAnalysis")
     sc.setLogLevel("WARN")
-
     ssc = StreamingContext(sc, micro_secs_batch)
     ssc.checkpoint(checkpoint_file)
-    kafkaStream = KafkaUtils.createStream(ssc, f"{zk_host}:{zk_port}", 'spark-streaming', {input_topic_name:1})
+    kafkaStream = KafkaUtils.createStream(ssc, "{zk_host}:{zk_port}".format(zk_host=zk_host, zk_port=zk_port), 'spark-streaming', {input_topic_name:1})
 
     parsed = kafkaStream.map(lambda v: json.loads(v[1]))
     enriched_tweets = parsed.map(lambda v: enrich_twitter_mc(mc_key, v))
     if output_file:
-        (parsed.map(lambda x: json.dumps(x))).saveAsTextFiles(output_file)
+        (enriched_tweets).saveAsTextFiles(output_file)
+    else:
+        (enriched_tweets).pprint()
     ssc.start()
     ssc.awaitTermination()
 
 
 if __name__ == '__main__':
+    meaning_cloud_key = ''
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--topic", help="Kafka topic output stream", required=True)
     parser.add_argument("-m", "--micro-batch", help="Micro batch in seconds. Default: 10 secs", type=int, default=10)
     parser.add_argument("-j", "--json-file", help="Output JSON file where to put the received tweets", default=None)
     args = parser.parse_args()
-    main(args.topic, micro_secs_batch=args.micro_batch, output_file=args.json_file)
+    main(args.topic, meaning_cloud_key, micro_secs_batch=args.micro_batch, output_file=args.json_file)
