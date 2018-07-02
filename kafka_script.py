e46597fe5208a700db7eeae3cef78b567098cba6
Teresa Fontanella De Santis <tere.fds@hotmail.com>
2018-07-01 23:43:09 -0300

    Make kafka script python2 compatible

--- kafka_script.py
+++ kafka_script.py
@@ -3,7 +3,7 @@ from kafka import KafkaProducer
 class KafkaSender(object):
 
     def __init__(self, host='localhost', port=9092):
-        self.producer = KafkaProducer(bootstrap_servers=f"{host}:{port}")
+        self.producer = KafkaProducer(bootstrap_servers="{host}:{port}".format(host=host, port=port))
 
     def send_message(self, topic, message):
         self.producer.send(topic, message.encode('utf-8'))
